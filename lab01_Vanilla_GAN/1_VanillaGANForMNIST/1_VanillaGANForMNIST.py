ba9daf09f10c70ba88df435f69e5ec0c626612b1
lionem2018 <lionem2018@gmail.com>
2019-03-07 16:10:28 +0900

    lab01 Vanilla GAN (add Vanilla GAN example for MNIST)

--- lab01_Vanilla_GAN/1_VanillaGANForMNIST/1_VanillaGANForMNIST.py
+++ lab01_Vanilla_GAN/1_VanillaGANForMNIST/1_VanillaGANForMNIST.py
@@ -6,42 +6,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("./MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets("./mnist/data/", one_hot=True)
 
-#########
+##########
 # 옵션 설정
-######
+##########
 total_epoch = 100
 batch_size = 100
 learning_rate = 0.0002
 # 신경망 레이어 구성 옵션
 n_hidden = 256
 n_input = 28 * 28
-n_noise = 128  # 생성기의 입력값으로 사용할 노이즈의 크기
+n_noise = 128  # Generator의 입력값으로 사용할 노이즈의 크기
 
-#########
+##########
 # 신경망 모델 구성
-######
+##########
 # GAN 도 Unsupervised 학습이므로 Autoencoder 처럼 Y 를 사용하지 않습니다.
 X = tf.placeholder(tf.float32, [None, n_input])
 # 노이즈 Z를 입력값으로 사용합니다.
 Z = tf.placeholder(tf.float32, [None, n_noise])
 
-# 생성기 신경망에 사용하는 변수들입니다.
+# Generator 신경망에 사용하는 변수들입니다.
 G_W1 = tf.Variable(tf.random_normal([n_noise, n_hidden], stddev=0.01))
 G_b1 = tf.Variable(tf.zeros([n_hidden]))
 G_W2 = tf.Variable(tf.random_normal([n_hidden, n_input], stddev=0.01))
 G_b2 = tf.Variable(tf.zeros([n_input]))
 
-# 판별기 신경망에 사용하는 변수들입니다.
+# Discriminator 신경망에 사용하는 변수들입니다.
 D_W1 = tf.Variable(tf.random_normal([n_input, n_hidden], stddev=0.01))
 D_b1 = tf.Variable(tf.zeros([n_hidden]))
-# 판별기의 최종 결과값은 얼마나 진짜와 가깝냐를 판단하는 한 개의 스칼라값입니다.
+# Discriminator의 최종 결과값은 얼마나 진짜와 가깝냐를 판단하는 한 개의 스칼라값입니다.
 D_W2 = tf.Variable(tf.random_normal([n_hidden, 1], stddev=0.01))
 D_b2 = tf.Variable(tf.zeros([1]))
 
 
-# 생성기(G) 신경망을 구성합니다.
+# Generator(G) 신경망을 구성합니다.
 def generator(noise_z):
     hidden = tf.nn.relu(
                     tf.matmul(noise_z, G_W1) + G_b1)
@@ -51,7 +51,7 @@ def generator(noise_z):
     return output
 
 
-# 판별기(D) 신경망을 구성합니다.
+# Discriminator(D) 신경망을 구성합니다.
 def discriminator(inputs):
     hidden = tf.nn.relu(
                     tf.matmul(inputs, D_W1) + D_b1)
@@ -77,19 +77,19 @@ D_real = discriminator(X)
 # 다만 loss_D와 loss_G는 서로 연관관계가 있기 때문에 두 개의 손실값이 항상 같이 증가하는 경향을 보이지는 않을 것 입니다.
 # loss_D가 증가하려면 loss_G는 하락해야하고, loss_G가 증가하려면 loss_D는 하락해야하는 경쟁관계에 있기 때문입니다.
 # 논문의 수식에 따른 다음 로직을 보면 loss_D 를 최대화하기 위해서는 D_gene 값을 최소화하게 됩니다.
-# 판별기에 진짜 이미지를 넣었을 때에도 최대값을 : tf.log(D_real)
+# Discriminator에 진짜 이미지를 넣었을 때에도 최대값을 : tf.log(D_real)
 # 가짜 이미지를 넣었을 때에도 최대값을 : tf.log(1 - D_gene)
 # 갖도록 학습시키기 때문입니다.
-# 이것은 판별기는 생성기가 만들어낸 이미지가 가짜라고 판단하도록 판별기 신경망을 학습시킵니다.
+# 이것은 Discriminator는 Generator가 만들어낸 이미지가 가짜라고 판단하도록 Discriminator 신경망을 학습시킵니다.
 loss_D = tf.reduce_mean(tf.log(D_real) + tf.log(1 - D_gene))
 # 반면 loss_G 를 최대화하기 위해서는 D_gene 값을 최대화하게 되는데,
-# 이것은 가짜 이미지를 넣었을 때, 판별기가 최대한 실제 이미지라고 판단하도록 생성기 신경망을 학습시킵니다.
-# 논문에서는 loss_D 와 같은 수식으로 최소화 하는 생성기를 찾지만,
+# 이것은 가짜 이미지를 넣었을 때, Discriminator가 최대한 실제 이미지라고 판단하도록 Generator 신경망을 학습시킵니다.
+# 논문에서는 loss_D 와 같은 수식으로 최소화 하는 Generator를 찾지만,
 # 결국 D_gene 값을 최대화하는 것이므로 다음과 같이 사용할 수 있습니다.
 loss_G = tf.reduce_mean(tf.log(D_gene))
 
-# loss_D 를 구할 때는 판별기 신경망에 사용되는 변수만 사용하고,
-# loss_G 를 구할 때는 생성기 신경망에 사용되는 변수만 사용하여 최적화를 합니다.
+# loss_D 를 구할 때는 Discriminator 신경망에 사용되는 변수만 사용하고,
+# loss_G 를 구할 때는 Generator 신경망에 사용되는 변수만 사용하여 최적화를 합니다.
 D_var_list = [D_W1, D_b1, D_W2, D_b2]
 G_var_list = [G_W1, G_b1, G_W2, G_b2]
 
@@ -100,9 +100,9 @@ train_D = tf.train.AdamOptimizer(learning_rate).minimize(-loss_D,
 train_G = tf.train.AdamOptimizer(learning_rate).minimize(-loss_G,
                                                          var_list=G_var_list)
 
-#########
+##########
 # 신경망 모델 학습
-######
+##########
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
@@ -114,7 +114,7 @@ for epoch in range(total_epoch):
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
         noise = get_noise(batch_size, n_noise)
 
-        # 판별기와 생성기 신경망을 각각 학습시킵니다.
+        # Discriminator와 Generator 신경망을 각각 학습시킵니다.
         _, loss_val_D = sess.run([train_D, loss_D],
                                  feed_dict={X: batch_xs, Z: noise})
         _, loss_val_G = sess.run([train_G, loss_G],
@@ -124,9 +124,9 @@ for epoch in range(total_epoch):
           'D loss: {:.4}'.format(loss_val_D),
           'G loss: {:.4}'.format(loss_val_G))
 
-    #########
+    ##########
     # 학습이 되어가는 모습을 보기 위해 주기적으로 이미지를 생성하여 저장
-    ######
+    ##########
     if epoch == 0 or (epoch + 1) % 10 == 0:
         sample_size = 10
         noise = get_noise(sample_size, n_noise)
